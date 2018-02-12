5ad2a218967e53f34ea3b57ae72a800ed2ef6d1c
intelicoin <36385517+intelicoin@users.noreply.github.com>
2018-02-12 05:20:01 -1000

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -74,8 +74,7 @@ const char     MINER_CONFIG_FILE_NAME[]                      = "miner_conf.json"
 
 //TODO Put here the name of your currency
 const char     CRYPTONOTE_NAME[]                             = "InteliCoin";
-const char     GENESIS_COINBASE_TX_HEX[]                     = "";
-
+const char     GENESIS_COINBASE_TX_HEX[]                     = "013c01ff0001ffffffffffff0f029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd0880712101d6b3f5d5c9219c7bb9872aecce8d79a8afbf7aad60a4c062ca76ac3af094b980";
 const uint8_t  CURRENT_TRANSACTION_VERSION                   =  1;
 const uint8_t  BLOCK_MAJOR_VERSION_1                         =  1;
 const uint8_t  BLOCK_MINOR_VERSION_0                         =  0;
